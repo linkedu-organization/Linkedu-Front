141966e1aeaed7cce09ddf97c09e59d40c14b94c
rayanne macedo <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2025-12-09 20:17:49 -0300

    Revert "Componentes e tela base"

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
+import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
