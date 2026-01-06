9c4d9169c61297ccf330dae1545cbbb21b8f9e99
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2026-01-06 18:43:58 -0300

    feat: tela inicial de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "@pages/homePage";
+import RegistrationPage from "@pages/register";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<RegistrationPage />} />
     </Routes>
   </BrowserRouter>
 );
