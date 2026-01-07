b714469138115a4dc9145af1a74570409c2a0059
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2026-01-07 19:35:32 -0300

    feat: utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,81 @@
+import * as validator from "email-validator";
+import moment from "moment";
+import { DATE_FORMAT, DATE_PARSE_FORMAT } from "./date";
+
+export const isValueValid = (value: unknown) =>
+  value !== undefined && value !== null && value !== "";
+
+export const isMaxValue = (campo: string, tamanhoMax: number) => {
+  if (campo === undefined || campo === null) {
+    return true;
+  }
+
+  return campo.length <= tamanhoMax;
+};
+
+export const isMinValue = (campo: string, tamanhoMin: number) => {
+  if (campo === undefined || campo === null) {
+    return false;
+  }
+  return campo.length >= tamanhoMin;
+};
+
+export const isEmail = (email: string) => validator.validate(email);
+
+export const getValueByKey = (
+  value: string,
+  list = [],
+  key = "value",
+  label = "text"
+) => {
+  let result = "-";
+  const filtered = list.filter((item) => item[key] === value);
+  if (filtered.length > 0) {
+    result = filtered[0][label];
+  }
+  return result;
+};
+
+export const getMultipleValuesByKey = (
+  values = [],
+  list = [],
+  key = "value",
+  label = "text"
+) => values.map((v) => getValueByKey(v, list, key, label)).join(", ");
+
+export const getFormatMonthYear = (dataDesejavel: string) => {
+  if (dataDesejavel) {
+    const partes = dataDesejavel.split("-");
+    const [ano, mes] = partes;
+
+    return ano ? `${mes}/${ano}` : "-";
+  }
+  return "-";
+};
+
+export const getValueDate = (
+  value: string,
+  formatter = DATE_FORMAT,
+  parser = DATE_PARSE_FORMAT
+) => (value ? moment(value, parser).format(formatter) : "-");
+
+export const getValue = (value: unknown) => value || "-";
+
+export const getFirstAndLastName = (name: string) => {
+  const fullName = name.split(" ");
+  return (
+    fullName.shift() + (fullName && fullName.length ? ` ${fullName.pop()}` : "")
+  ).toUpperCase();
+};
+
+export const parseBoolean = (value: boolean) => {
+  if (value) return "Sim";
+  return "Não";
+};
+
+export const formatTypedValue = (value: string, maxSize: number) => {
+  if (value.length > maxSize) {
+    return `${value.substring(0, maxSize - 5)}...`;
+  }
+  return value;
+};
