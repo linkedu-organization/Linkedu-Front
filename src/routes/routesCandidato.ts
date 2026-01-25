e4e80206f34b20ffd165c27d37f267b4511bb2ed
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2026-01-24 21:21:38 -0300

    chore: ajusta rotas

--- src/routes/routesCandidato.ts
+++ src/routes/routesCandidato.ts
@@ -2,30 +2,30 @@ import type { Candidato } from "@domains/Candidato";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/candidatos`,
 });
 
 export const registerCandidato = async (data: Candidato) => {
-  const response = await api.post("/candidato/", data);
+  const response = await api.post("/", data);
   return response.data;
 };
 
 export const getCandidato = async (id: number) => {
-  const response = await api.get(`/candidato/${id}`);
+  const response = await api.get(`/${id}`);
   return response.data;
 };
 
 export const getAllCandidato = async () => {
-  const response = await api.get("/candidato/");
+  const response = await api.get("/");
   return response.data;
 };
 
 export const updateCandidato = async (id: number, data: Candidato) => {
-  const response = await api.put(`/candidato/${id}`, data);
+  const response = await api.put(`/${id}`, data);
   return response.data;
 };
 
 export const deleteCandidato = async (id: number) => {
-  const response = await api.delete(`/candidato/${id}`);
+  const response = await api.delete(`/${id}`);
   return response.data;
 };
